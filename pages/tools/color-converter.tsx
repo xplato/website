3e9fcb9f5ab99bd997c094fa540e5f541346a988
Tristan B <tristan@infinium.earth>
2022-04-17 22:00:26 -0600

    ui: Add copy status to Color converter

--- pages/tools/color-converter.tsx
+++ pages/tools/color-converter.tsx
@@ -10,13 +10,18 @@ import Button from '../../components/Button';
 
 import { hexToRGB, RGBToHex } from '../../logic/tools/color_converter';
 import { copyToClipboard } from '../../logic/utils';
-
+import { CheckIcon, XIcon } from '@heroicons/react/solid';
 
 const ColorConverter = () => {
 	const [result, setResult] = useState('');
+	const previousResult = usePrevious(result);
+
 	const [value, setValue] = useState('');
+	const [copyStatus, setCopyStatus] = useState<string | undefined>(undefined);
+
 	const [mode, setMode] = useState<'hexToRGB' | 'RGBToHex'>('hexToRGB');
 	const previousMode = usePrevious(mode);
+
 
 	const onSelectChange = (v: any) => {
 		setMode(v.id);
@@ -27,12 +32,21 @@ const ColorConverter = () => {
 	};
 
 	const onResultClick = () => {
-		copyToClipboard(result);
-	}
+		setCopyStatus('pending');
+
+		copyToClipboard(result).then(
+			function () {
+				setCopyStatus('success');
+			},
+			function () {
+				setCopyStatus('fail');
+			}
+		);
+	};
 
 	const getRandomHex = () => {
-		return `#${Math.floor(Math.random()*16777215).toString(16)}`;
-	}
+		return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	};
 
 	const [randomColor, setRandomColor] = useState(getRandomHex());
 
@@ -44,14 +58,14 @@ const ColorConverter = () => {
 		setValue('');
 		setResult('');
 		setRandomColor(getRandomHex());
-	}
+	};
 
 	useEffect(() => {
 		if (mode === 'hexToRGB') {
 			if (value.includes('#')) {
 				setValue((v) => String(v).replace('#', ''));
 			}
-	
+
 			if (value.length > 6) {
 				setValue((v) => String(v).slice(0, 6));
 			}
@@ -66,13 +80,9 @@ const ColorConverter = () => {
 		if (mode === 'RGBToHex' && value) {
 			let v: RegExpMatchArray | null | number[] = value.match(/\d+/g);
 			if (!v || v.length < 3) return;
-			v = v.map(e => Number(e));
+			v = v.map((e) => Number(e));
 
-			setResult(RGBToHex(
-				v[0],
-				v[1],
-				v[2]
-			));
+			setResult(RGBToHex(v[0], v[1], v[2]));
 		}
 	}, [value, mode]);
 
@@ -81,6 +91,12 @@ const ColorConverter = () => {
 			setValue('');
 		}
 	}, [mode, previousMode]);
+
+	useEffect(() => {
+		if (result !== previousResult) {
+			setCopyStatus(undefined);
+		}
+	}, [result]);
 
 	return (
 		<>
@@ -99,8 +115,14 @@ const ColorConverter = () => {
 					<div className='ui radius-8 border-ui-2 shadow-xs'>
 						<div className='w-100p flex-sb px-1-5r py-1r border-bottom-ui-2'>
 							<h5 className='mb-0 mt-0'>Convert Colors</h5>
-							<Button.Action className='hy-button icon-only' onClick={refreshRandomColor} title='Randomize color'>
-								<i className="j-icon"><RefreshIcon /></i>
+							<Button.Action
+								className='hy-button icon-only'
+								onClick={refreshRandomColor}
+								title='Randomize color'
+							>
+								<i className='j-icon'>
+									<RefreshIcon />
+								</i>
 							</Button.Action>
 						</div>
 						<div className='p-1-5r border-bottom-ui-2 flex align-c justify-c flex-row portrait-flex-c'>
@@ -120,11 +142,23 @@ const ColorConverter = () => {
 								onChange={onSelectChange}
 							/>
 						</div>
-						<div className="p-1-5r flex-c text-c">
+						<div className='p-1-5r flex-c text-c'>
 							{result ? (
-								<Button.Action onClick={onResultClick}>{result}</Button.Action>
+								<div className='flex align-c justify-c flex-row'>
+									<Button.Action onClick={onResultClick}>
+										Copy — {result}
+									</Button.Action>
+									<i className='j-icon ml-0-5r'>
+										{copyStatus === 'success' && (
+											<CheckIcon />
+										)}
+										{copyStatus === 'fail' && <XIcon />}
+									</i>
+								</div>
 							) : (
-								<span className='caption-text sm-ls'>Random: {getColor()}</span>
+								<span className='caption-text sm-ls'>
+									Random: {getColor()}
+								</span>
 							)}
 						</div>
 					</div>
