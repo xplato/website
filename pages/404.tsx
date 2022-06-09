fb11729f89ff091f9e7b6e748e7656ac18078580
Tristan B <tristan@infinium.earth>
2022-06-09 01:33:10 -0600

    ui: Improve 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,40 +1,87 @@
-import Image from 'next/image';
-import { isMobile } from 'react-device-detect';
-
-import Entity from '../components/Entity';
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
+import ELi from '../components/ELi';
 import Li from '../components/Li';
+import Title from '../components/Title';
 
-import { variants, transition } from '../logic/motion';
+interface BlockProps {
+	index: number;
+	letter: string;
+	bg: BG;
+	setBg: Dispatch<SetStateAction<BG>>;
+}
+
+interface BG {
+	left: number;
+	width: number;
+}
+
+const Block = ({ index, letter, bg, setBg }: BlockProps) => {
+	const ref = useRef<HTMLDivElement>();
+
+	useEffect(() => {
+		if (index === 0 && bg.width === 0) {
+			makeCurrent();
+		}
+	}, [index, bg]);
+
+	const makeCurrent = () => {
+		if (typeof ref.current !== 'undefined') {
+			const el = ref.current;
+
+			setBg((bg) => ({
+				...bg,
+				left: el.offsetLeft,
+				width: el.offsetWidth,
+			}));
+		}
+	};
+
+	return (
+		<div
+			// @ts-ignore
+			ref={ref}
+			className='block'
+			onMouseOver={makeCurrent}
+		>
+			<span>{letter}</span>
+		</div>
+	);
+};
 
 const FourOhFour = () => {
+	const [bg, setBg] = useState<BG>({
+		left: 0,
+		width: 179,
+	});
+
 	return (
 		<>
-			<section className='section p-0 flex-c special-section'>
-				<div className='container flex-c h-100p'>
-					<Entity
-						variants={variants.fadeInUp}
-						transition={{
-							...transition.default,
-							delay: 0.2,
+			<section className='four-oh-four'>
+				<div className='wrapper'>
+					<div
+						className='bg'
+						style={{
+							left: `${bg.left}px`,
+							width: `${bg.width}px`,
 						}}
-						className='h-100p'
-					>
-						<div className='text-container h-100p flex-c text-c'>
-							<h1 className='fs-10xl tablet-fs-9xl landscape-fs-7xl portrait-fs-6xl fw-700 flex align-c justify-c flex-row text-c'>
-								4{' '}
-								<Image
-									src='/images/etc/ghost.png'
-									width={isMobile ? 75 : 150}
-									height={isMobile ? 100 : 200}
-								/>{' '}
-								4
-							</h1>
-							<p className='fs-lg'>
-								This is some spooky shit, man. Try heading back home.
-							</p>
-							<Li href='/' className='button'>Go home</Li>
-						</div>
-					</Entity>
+					></div>
+					<Block index={0} letter='4' bg={bg} setBg={setBg} />
+					<Block index={0} letter='0' bg={bg} setBg={setBg} />
+					<Block index={0} letter='4' bg={bg} setBg={setBg} />
+				</div>
+
+				<div className='mw-35r flex-c text-c mt-3r ml-mt-1-5r'>
+					{/* <Title>Page not found</Title> */}
+					<p className='fs-lg mb-2r'>
+						The page you requested was not found on this server. If
+						you think this is a mistake, please{' '}
+						<ELi href='https://github.com/xplato/website/issues'>
+							open an issue.
+						</ELi>{' '}
+						Otherwise, go ahead and head back to the homepage.
+					</p>
+
+					<Li href='/' className='fluid-button'>Go back home</Li>
 				</div>
 			</section>
 		</>
---
+++ pages/crystal/index.tsx
@@ -0,0 +1,30 @@
+import PageMeta from '../../components/PageMeta';
+
+import Hero from '../../sections/crystal/Hero';
+import Introduction from '../../sections/crystal/Introduction';
+import OSTeaserGallery from '../../sections/crystal/OSTeaserGallery';
+import Project from '../../sections/crystal/Project';
+import References from '../../sections/crystal/References';
+
+const Home = () => {
+	return (
+		<>
+			<PageMeta
+				meta={{
+					title: 'Crystal',
+					description: 'Experimenting in futuristic UI/UX',
+				}}
+			/>
+
+			<div className='expose'>
+				<Hero />
+				<Introduction />
+				<OSTeaserGallery />
+				<Project />
+				<References />
+			</div>
+		</>
+	);
+};
+
+export default Home;
